93328272ffdaf7241f70b687b13f39a1b1ef280f
Ed Stephenson <edward.stephenson@me.com>
2021-04-20 14:20:43 +0100

    breakdown edi payload

--- src/payloads/edi/edi.payload.ts
+++ src/payloads/edi/edi.payload.ts
@@ -1,11 +1,17 @@
 import { AddressEntityInterface } from '../../entities/address/address.entity';
 import { LineItemEntityInterface } from '../../entities/line-item/line-item.entity';
 
+export type EdiPayloadShippingAddressDto = Required<
+  Omit<AddressEntityInterface, 'company'>
+>;
+
+export type EdiPayloadLineItemDto = LineItemEntityInterface;
+
 export interface EdiPayloadDto {
   poNumber: string;
   date: string;
   currency: string;
   email: string;
-  shippingAddress: Required<Omit<AddressEntityInterface, 'company'>>;
-  lineItems: LineItemEntityInterface[];
+  shippingAddress: EdiPayloadShippingAddressDto;
+  lineItems: EdiPayloadLineItemDto[];
 }
