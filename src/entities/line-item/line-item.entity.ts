bc9b735a8558270a505e5db79730f0304f57555c
Ed Stephenson <edward.stephenson@me.com>
2021-06-25 09:59:27 +0100

    add price to line item

--- src/entities/line-item/line-item.entity.ts
+++ src/entities/line-item/line-item.entity.ts
@@ -4,6 +4,7 @@ export class LineItemEntityInterface {
   sku: string;
   description: string;
   quantity: number;
+  price?: number;
 }
 
 export type LineItemEntity = BaseEntity & LineItemEntityInterface;
