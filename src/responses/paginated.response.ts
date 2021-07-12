be960adcc2711e9f41e8d01a6ef5b89fbfc97b67
Ed Stephenson <edward.stephenson@me.com>
2021-07-12 13:05:44 +0100

    improve pagination response data

--- src/responses/paginated.response.ts
+++ src/responses/paginated.response.ts
@@ -1,5 +1,10 @@
 export interface PaginationResponse<Entity> {
   results: Entity[];
+  page: number;
+  totalPages: number;
+  start: number;
+  end: number;
+  pageLength: number;
   filteredCount: number;
   totalCount: number;
 }
