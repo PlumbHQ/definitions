25f7885db32de3c33911ab2f6966308fe74cb405
Ed Stephenson <edward.stephenson@me.com>
2021-08-01 17:52:37 +0100

    remove shopName

--- src/entities/order/order.entity.ts
+++ src/entities/order/order.entity.ts
@@ -2,7 +2,6 @@ import { BaseEntity, DatesEntity } from '../base.entity';
 
 export class OrderEntityInterface {
   uuid: string; // Internal generated UUID for the order
-  shopName: string; // Shopify Name of the Order
 }
 
 export type OrderEntity = BaseEntity & OrderEntityInterface & DatesEntity;
