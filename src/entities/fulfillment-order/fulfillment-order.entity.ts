c6015ba33220cf109c6c7534a36aa7a2e9fc16e0
Ed Stephenson <edward.stephenson@me.com>
2021-09-05 11:02:46 +0100

    improve FulfillmentOrderRequestStatus

--- src/entities/fulfillment-order/fulfillment-order.entity.ts
+++ src/entities/fulfillment-order/fulfillment-order.entity.ts
@@ -5,35 +5,33 @@ import { LineItemEntity } from '../line-item/line-item.entity';
 import { ShipmentEntity } from '../shipment/shipment.entity';
 
 export type FulfillmentOrderStatus =
+  | 'scheduled' // The fulfillment order is deferred and will be ready for fulfillment after the datetime specified in fulfill_at.
   | 'open' // The fulfillment order is ready for fulfillment.
   | 'in_progress' // The fulfillment order is being processed.
-  | 'scheduled' // The fulfillment order is deferred and will be ready for fulfillment after the datetime specified in fulfill_at.
-  | 'cancelled' // The fulfillment order has been cancelled by the merchant.
+  | 'cancelled' // The fulfillment order has been cancelled by PlumbHQ.
   | 'incomplete' // The fulfillment order cannot be completed as requested.
   | 'closed'; // The fulfillment order has been completed and closed.
 
 export enum FulfillmentOrderStatusEnum {
+  Scheduled = 'scheduled',
   Open = 'open',
   InProgress = 'in_progress',
-  Scheduled = 'scheduled',
   Cancelled = 'cancelled',
   Incomplete = 'incomplete',
   Closed = 'closed',
 }
 
 export type FulfillmentOrderRequestStatus =
-  | 'unsubmitted' // The initial state for newly created fulfillment orders. This is the only valid state for fulfillment orders not assigned to a fulfillment service.
-  | 'submitted' // The merchant requested fulfillment for this fulfillment order.
-  | 'accepted' // The fulfillment service accepted the merchant's fulfillment request.
-  | 'rejected' // The fulfillment service rejected the merchant's fulfillment request.
-  | 'closed'; // The fulfillment service closed the fulfillment order without completing it.
+  | 'unsubmitted' // The initial state for a newly created FulfillmentOrder.
+  | 'submitted' // PlumbHQ has requested fulfillment from the supplier but they have not responded.
+  | 'accepted' // The supplier accepted the PlumbHQ's request.
+  | 'rejected'; // The supplier rejected the PlumbHQ's request.
 
 export enum FulfillmentOrderRequestStatusEnum {
   Unsubmitted = 'unsubmitted',
   Submitted = 'submitted',
   Accepted = 'accepted',
   Rejected = 'rejected',
-  Closed = 'closed',
 }
 
 export type FulfillmentOrderRequestType =
